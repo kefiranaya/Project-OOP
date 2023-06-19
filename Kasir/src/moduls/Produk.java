cc578233cf1700aa3146b929f572c5de938ec2b4
kefiranaya <113886919+kefiranaya@users.noreply.github.com>
2023-06-19 14:53:44 +0800

    Add files via upload

---
+++ Kasir/src/moduls/Produk.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package moduls;
+
+/**
+ *
+ * @author kefiraanaya
+ */
+public class Produk {
+    private int id;
+    private String name;
+    private long harga;
+    private int jumlah;
+    
+    public Produk(int id, String name, long price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.harga = harga;
+        this.jumlah = jumlah;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getHarga() {
+        return harga;
+    }
+
+    public void setHarga(long harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+}
